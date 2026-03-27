b9971165a0645777b2fe62a36281bff2a9bbdc51
Assol-Zakirova <zakirovaassol@gmail.com>
2026-03-27 14:00:30 +0600

    month6 hw1

--- product/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from product.models import Category, Product, Review, UserConfirmation 
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Review)
-admin.site.register(UserConfirmation)
